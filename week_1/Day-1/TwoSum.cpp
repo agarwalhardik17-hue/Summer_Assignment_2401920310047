23ee6ae0e706250f1a4dfcf27f63663f2c5ad4e9
Hardik Agarwal <agarwalhardik17@gmail.com>
2026-06-07 13:32:36 +0530

    Create twoSum

--- week_1/Day-1/TwoSum.cpp
+++ week_1/Day-1/TwoSum.cpp
@@ -1,38 +1,19 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
+
         vector<int> ans;
+        int n = nums.size();
 
-        for(int i = 0; i < n-1; i++){
-
-            int val = target - nums[i];
-
+        for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
-
-                if(val == nums[j]){
+                if(nums[i] + nums[j] == target){
                     ans.push_back(i);
                     ans.push_back(j);
-
                 }
             }
         }
         return ans;
+
     }
 };
-
-int main(){
-    Solution s;
-    vector<int> nums = {2,7,10,15};
-    int target = 9;
-
-    vector<int> ans = s.twoSum(nums,target);
-
-    for(int i : ans){
-        cout<<i<<" ";
-    }
-}
