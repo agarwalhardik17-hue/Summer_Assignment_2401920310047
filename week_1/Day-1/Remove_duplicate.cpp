9233a05a612fa43e9586bc62859954ca6231df90
Hardik Agarwal <agarwalhardik17@gmail.com>
2026-06-07 13:35:18 +0530

    Create removeDuplicates
    Refactor removeDuplicates function to improve readability and efficiency.

--- week_1/Day-1/Remove_duplicate.cpp
+++ week_1/Day-1/Remove_duplicate.cpp
@@ -1,37 +1,14 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0;
-        int j = 1;
-        int n = nums.size();
-
-        while(j<n){
-
-            if(nums[i] != nums[j]){
-                i++;
-                nums[i] = nums[j];
-            }
-            j++;
-        }
-        return i+1;
         
+        int j = 1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
 };
-
-int main(){
-    Solution s;
-    vector<int> nums = {1,1,2,2,3,4,4,5};
-
-    int ans = s.removeDuplicates(nums);
-
-    cout<<"Length of array after removing duplicates: "<<ans<<endl;
-
-    cout<<"Array after removing duplicates: ";
-    for(int i=0; i<ans; i++){
-        cout<<nums[i]<<" ";
-    }
-}
